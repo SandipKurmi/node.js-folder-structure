a4031c3e21605cd8aec2bf264ec3fea298208900
kurmicshare <kurmi.cshare@gmail.com>
2023-06-17 09:28:14 +0530

    remove cashing from node js

--- src/models/model.js
+++ src/models/model.js
@@ -1,15 +1,3 @@
 import mongoose, { Schema } from 'mongoose';
-import cachegoose from 'recachegoose';
 
-cachegoose(mongoose, {
-  engine: 'memory',
-});
-// cachegoose(mongoose, {
-//     engine: 'redis',
-//     port: 6379,
-//     host: 'localhost'
-// });
 module.exports = { mongoose: mongoose, Schema: Schema };
-
-// export const Mongoose = mongoose;
-// export const schema = Schema;
